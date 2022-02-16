8e116626ea4599129d28b8720c530c0235a1429a
AlexVild <ajv2324@gmail.com>
2022-02-16 11:50:24 -0500

    Alex: Can now properly store exp and level of jewels in the player DB

---
+++ Intersect.Server/Migrations/20220216164525_ItemEnchantingInitialMigration.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Intersect.Server.Migrations
+{
+    public partial class ItemEnchantingInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Exp",
+                table: "Player_Items",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level",
+                table: "Player_Items",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Exp",
+                table: "Player_Bank",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level",
+                table: "Player_Bank",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Exp",
+                table: "Guild_Bank",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level",
+                table: "Guild_Bank",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Exp",
+                table: "Bag_Items",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level",
+                table: "Bag_Items",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Exp",
+                table: "Player_Items");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "Player_Items");
+
+            migrationBuilder.DropColumn(
+                name: "Exp",
+                table: "Player_Bank");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "Player_Bank");
+
+            migrationBuilder.DropColumn(
+                name: "Exp",
+                table: "Guild_Bank");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "Guild_Bank");
+
+            migrationBuilder.DropColumn(
+                name: "Exp",
+                table: "Bag_Items");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "Bag_Items");
+        }
+    }
+}
