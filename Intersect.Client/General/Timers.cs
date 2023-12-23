7f9f291518e62fc2e58b791d1cf17845f515dbdb
AlexVild <ajv2324@gmail.com>
2023-12-23 15:23:04 -0500

    fix: (Day) NRE in timer window

--- Intersect.Client/General/Timers.cs
+++ Intersect.Client/General/Timers.cs
@@ -8,6 +8,12 @@ namespace Intersect.Client.General
     {
         public static List<Timer> ActiveTimers = new List<Timer>();
 
-        public static List<Timer> DisplayableTimers => ActiveTimers.Where(t => !t.Descriptor.OnlyDisplayOnExclusiveMaps || t.Descriptor.ContainsExclusiveMap(Globals.Me.MapInstance.Id)).ToList();
+        public static List<Timer> DisplayableTimers => ActiveTimers.Where(t => {
+            if (t.Descriptor == null || Globals.Me == null || Globals.Me.MapInstance == null)
+            {
+                return false;
+            }
+            return !t.Descriptor.OnlyDisplayOnExclusiveMaps || t.Descriptor.ContainsExclusiveMap(Globals.Me.MapInstance.Id);
+        }).ToList();
     }
 }
